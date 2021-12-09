d7dd96abad66cc45a79e7f24b45a792d410f9501
Guilherme Souza <32180229+gqgs@users.noreply.github.com>
2021-12-09 13:31:17 -0300

    day9: keep largest in heap

--- day9.go
+++ day9.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"os"
-	"sort"
 	"strings"
 	"sync"
 )
@@ -17,6 +17,24 @@ func min(a, b byte) byte {
 
 func castRune(c byte) int {
 	return int(c - '0')
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
 func findMinPoints(input []string) [][]int {
@@ -44,7 +62,7 @@ func gold(input []string, points [][]int) int {
 	mu := new(sync.Mutex)
 	wg := new(sync.WaitGroup)
 	wg.Add(len(points))
-	var basins []int
+	basins := IntHeap{0, 0, 0}
 	for _, point := range points {
 		go func(point []int) {
 			defer wg.Done()
@@ -56,16 +74,16 @@ func gold(input []string, points [][]int) int {
 				{y + 1, x},
 			})
 			mu.Lock()
-			basins = append(basins, size)
+			if size > basins[0] {
+				heap.Pop(&basins)
+				heap.Push(&basins, size)
+			}
 			mu.Unlock()
 		}(point)
 	}
 	wg.Wait()
 
-	sort.Ints(basins)
-
-	l := len(basins)
-	return basins[l-1] * basins[l-2] * basins[l-3]
+	return basins[0] * basins[1] * basins[2]
 }
 
 func basinSize(input []string, stack [][]int) int {
