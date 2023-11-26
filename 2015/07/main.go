9574970e0b36c890c62a8533845905a6f49ca461
Guilherme <gqgs@protonmail.com>
2023-11-26 03:09:26 -0300

    Update

--- 2015/07/main.go
+++ 2015/07/main.go
@@ -13,12 +13,89 @@ type Operand string
 type Instruction string
 type State map[Operand]uint16
 
+// id -> dependencies
+type Graph map[string][]string
+
+func dependencyGraph(intructions []Instruction, g Graph) Graph {
+	if len(intructions) == 0 {
+		return g
+	}
+
+	next := intructions[0]
+	g[string(next)] = next.Dependencies()
+	return dependencyGraph(intructions[1:], g)
+}
+
 func (o Operand) Value(state State) uint16 {
 	result, err := strconv.ParseUint(string(o), 10, 16)
 	if err == nil {
 		return uint16(result)
 	}
 	return state[o]
+}
+
+func (o Operand) IsIdentifier() bool {
+	_, err := strconv.ParseUint(string(o), 10, 16)
+	return err != nil
+}
+
+func (i Instruction) Dependencies() (dependencies []string) {
+	switch {
+	case strings.Contains(string(i), "AND"):
+		var src1, src2, dst Operand
+		fmt.Sscanf(string(i), "%s AND %s -> %s", &src1, &src2, &dst)
+		if src1.IsIdentifier() {
+			dependencies = append(dependencies, string(src1))
+		}
+		if src2.IsIdentifier() {
+			dependencies = append(dependencies, string(src2))
+		}
+
+	case strings.Contains(string(i), "OR"):
+		var src1, src2, dst Operand
+		fmt.Sscanf(string(i), "%s OR %s -> %s", &src1, &src2, &dst)
+		if src1.IsIdentifier() {
+			dependencies = append(dependencies, string(src1))
+		}
+		if src2.IsIdentifier() {
+			dependencies = append(dependencies, string(src2))
+		}
+
+	case strings.Contains(string(i), "LSHIFT"):
+		var src1, src2, dst Operand
+		fmt.Sscanf(string(i), "%s LSHIFT %s -> %s", &src1, &src2, &dst)
+		if src1.IsIdentifier() {
+			dependencies = append(dependencies, string(src1))
+		}
+		if src2.IsIdentifier() {
+			dependencies = append(dependencies, string(src2))
+		}
+
+	case strings.Contains(string(i), "RSHIFT"):
+		var src1, src2, dst Operand
+		fmt.Sscanf(string(i), "%s RSHIFT %s -> %s", &src1, &src2, &dst)
+		if src1.IsIdentifier() {
+			dependencies = append(dependencies, string(src1))
+		}
+		if src2.IsIdentifier() {
+			dependencies = append(dependencies, string(src2))
+		}
+
+	case strings.HasPrefix(string(i), "NOT"):
+		var src, dst Operand
+		fmt.Sscanf(string(i), "NOT %s -> %s", &src, &dst)
+		if src.IsIdentifier() {
+			dependencies = append(dependencies, string(src))
+		}
+
+	default:
+		var src, dst Operand
+		fmt.Sscanf(string(i), "%s -> %s", &src, &dst)
+		if src.IsIdentifier() {
+			dependencies = append(dependencies, string(src))
+		}
+	}
+	return
 }
 
 func (i Instruction) Eval(state State) State {
@@ -77,7 +154,11 @@ func solve() error {
 		instuctions = append(instuctions, Instruction(scanner.Text()))
 	}
 
-	for key, value := range run(instuctions, make(State)) {
+	// for key, value := range run(instuctions, make(State)) {
+	// 	fmt.Println(key, value)
+	// }
+
+	for key, value := range dependencyGraph(instuctions, make(Graph)) {
 		fmt.Println(key, value)
 	}
 
