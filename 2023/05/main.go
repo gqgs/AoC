eb859f7c928797964d9a30e1f461f00773a91ac6
Guilherme <gqgs@protonmail.com>
2023-12-17 00:21:25 -0300

    2023 05

--- 2023/05/main.go
+++ 2023/05/main.go
@@ -7,12 +7,22 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+
+	"github.com/gqgs/AoC2021/generic"
 )
 
 type Map struct {
 	Destination int
 	Source      int
 	Range       int
+}
+
+func (m Map) Left() int {
+	return m.Source
+}
+
+func (m Map) Right() int {
+	return m.Source + m.Range
 }
 
 func (m Map) transform(n int) (int, bool) {
@@ -22,39 +32,122 @@ func (m Map) transform(n int) (int, bool) {
 	return n, false
 }
 
-func silver(seeds []int, maps []Map, mapSizes []int) []int {
+func seedValue(seed int, maps []Map, mapSizes []int) int {
 	var i int
 	for _, r := range mapSizes {
-		var newSeeds []int
-	Next:
-		for _, s := range seeds {
-			for _, m := range maps[i : i+r] {
-				t, inRange := m.transform(s)
-				if inRange {
-					newSeeds = append(newSeeds, t)
-					continue Next
-				}
+		for _, m := range maps[i : i+r] {
+			t, inRange := m.transform(seed)
+			if inRange {
+				seed = t
+				break
 			}
-			newSeeds = append(newSeeds, s)
 		}
 		i += r
-		seeds = newSeeds
+	}
+	return seed
+}
+
+func silver(seeds []int, maps []Map, mapSizes []int) []int {
+	for i, seed := range seeds {
+		seeds[i] = seedValue(seed, maps, mapSizes)
 	}
 	return seeds
 }
 
-func gold(seeds []int, maps []Map, mapSizes []int) []int {
-	return silver(goldSeeds(seeds), maps, mapSizes)
+func transformRange(initMap Map, transformMaps []Map) (resultMaps []Map) {
+	remaining := make(generic.Queue[Map], 0)
+	remaining.Push(initMap)
+Next:
+	for len(remaining) > 0 {
+		rangeMap := remaining.Pop()
+		for _, transformMap := range transformMaps {
+			// 0
+			if transformMap.Left() <= rangeMap.Left() && transformMap.Right() >= rangeMap.Right() {
+				resultMaps = append(resultMaps, Map{
+					Source: transformMap.Destination - transformMap.Source + rangeMap.Left(),
+					Range:  rangeMap.Range,
+				})
+				continue Next
+			}
+
+			// 1
+			if transformMap.Left() < rangeMap.Left() && transformMap.Right() > rangeMap.Left() && transformMap.Right() < rangeMap.Right() {
+				resultMaps = append(resultMaps, Map{
+					Source: transformMap.Destination - transformMap.Source + rangeMap.Left(),
+					Range:  transformMap.Right() - rangeMap.Left(),
+				})
+				remaining.Push(Map{
+					Source: transformMap.Right(),
+					Range:  rangeMap.Right() - transformMap.Right(),
+				})
+				continue Next
+			}
+
+			// 2
+			if transformMap.Right() > rangeMap.Right() && transformMap.Left() < rangeMap.Right() && transformMap.Left() > rangeMap.Left() {
+				resultMaps = append(resultMaps, Map{
+					Source: transformMap.Destination - transformMap.Source + transformMap.Left(),
+					Range:  rangeMap.Right() - transformMap.Left(),
+				})
+				remaining.Push(Map{
+					Source: rangeMap.Left(),
+					Range:  transformMap.Left() - rangeMap.Left(),
+				})
+				continue Next
+			}
+
+			// 5
+			if transformMap.Left() > rangeMap.Left() && transformMap.Right() < rangeMap.Right() {
+				resultMaps = append(resultMaps, Map{
+					Source: transformMap.Destination - transformMap.Source + transformMap.Left(),
+					Range:  transformMap.Right() - transformMap.Left(),
+				})
+				remaining.Push(Map{
+					Source: rangeMap.Left(),
+					Range:  transformMap.Left() - rangeMap.Left(),
+				})
+				remaining.Push(Map{
+					Source: transformMap.Right(),
+					Range:  rangeMap.Right() - transformMap.Right(),
+				})
+				continue Next
+			}
+		}
+		resultMaps = append(resultMaps, rangeMap)
+	}
+
+	return
 }
 
-func goldSeeds(seeds []int) []int {
-	var newSeeds []int
-	for i := 0; i < len(seeds); i += 2 {
-		for j := seeds[i]; j < seeds[i]+seeds[i+1]; j++ {
-			newSeeds = append(newSeeds, j)
+func gold(seeds []int, maps []Map, mapSizes []int) []int {
+	rangeMaps := rangeMap(seeds)
+	var i int
+	for _, ms := range mapSizes {
+		var newRangeMaps []Map
+		for _, rm := range rangeMaps {
+			newRangeMaps = append(newRangeMaps, transformRange(rm, maps[i:i+ms])...)
 		}
+		rangeMaps = newRangeMaps
+		i += ms
 	}
-	return newSeeds
+
+	results := make([]int, len(rangeMaps))
+	for j, rm := range rangeMaps {
+		results[j] = rm.Source
+	}
+
+	return results
+}
+
+func rangeMap(seeds []int) []Map {
+	var rangeMap []Map
+	for i := 0; i < len(seeds); i += 2 {
+		rangeMap = append(rangeMap, Map{
+			Source: seeds[i],
+			Range:  seeds[i+1],
+		})
+	}
+	return rangeMap
 }
 
 func solve() error {
